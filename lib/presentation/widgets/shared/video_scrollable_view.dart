62ec256b76f3d5e6c90a5da3c45805485e2aa58c
avirgili <avirgilitech@gmail.com>
2023-08-25 00:13:13 -0400

    add video player and widget fullscreen player

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 
+import '../video/fullscreen_player.dart';
+
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
 
@@ -18,7 +20,10 @@ class VideoScrollableView extends StatelessWidget {
         return Stack(
           children: [
             //video player + gradiente
-
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                  caption: videoPost.caption, videoUrl: videoPost.videoUrl),
+            ),
             //video button
             Positioned(
                 bottom: 40, right: 15, child: VideoButtons(video: videoPost)),
---
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class FullScreenPlayer extends StatefulWidget {
+  final String videoUrl;
+  final String caption;
+
+  const FullScreenPlayer(
+      {super.key, required this.videoUrl, required this.caption});
+
+  @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    //cambiar luego a .network para cargar desde internet
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: controller.initialize(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(
+                child: CircularProgressIndicator(strokeWidth: 2));
+          }
+          return AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller),
+                Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      color: Colors.black.withOpacity(0.5),
+                      child: Text(widget.caption,
+                          style: const TextStyle(color: Colors.white)),
+                    ))
+              ],
+            ),
+          );
+        });
+  }
+}
